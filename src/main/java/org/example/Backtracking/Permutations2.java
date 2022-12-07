bc7a9b21be089577cbd838f74e5a441cd54c3614
Vivek Jha <vivekjha4u2@gmail.com>
2022-12-07 08:38:03 +0530

    Permutations2

---
+++ src/main/java/org/example/Backtracking/Permutations2.java
@@ -0,0 +1,40 @@
+package org.example.Backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutations2 {
+    public static void main(String[] args) {
+        int [] nums = new int[]{1,2,2};
+        List<List<Integer>> result = permutations2(nums);
+        for(List<Integer> i: result){
+            System.out.println(i);
+        }
+    }
+
+    private static List<List<Integer>> permutations2(int[] nums) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<Integer>(), nums, new boolean[nums.length] );
+        return list;
+    }
+
+    private static void backtrack(List<List<Integer>> list, ArrayList<Integer> tempList, int[] nums, boolean [] used) {
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        }else{
+
+            for(int i = 0; i < nums.length; i++){
+                if(used[i] || (i>0 && nums[i] == nums[i-1] && used[i-1])) continue;
+//                if(tempList.contains(nums[i])) continue;
+
+                used[i] = true;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, used);
+                used[i] = false;
+                tempList.remove(tempList.size()-1);
+            }
+        }
+    }
+}
